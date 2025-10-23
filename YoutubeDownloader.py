d9a9d52337e393bf02459c86af1dc4aa6aee26dc
alex1608-cell <alejc2006@gmail.com>
2025-10-23 20:18:41 +0200

    De momento solo redirige y el script en python esta hecho y funcionando

---
+++ YoutubeDownloader.py
@@ -0,0 +1,12 @@
+from pytubefix import YouTube
+
+link = input("Link de youtube : ")
+
+print("Descargando...")
+
+yt = YouTube(link)
+
+yt.streams.get_highest_resolution().download()
+
+print("El video se descargo correctamente :3")
+ # Aqui esta el archivo Python para descargar videos y funciona
---
+++ script.ts
@@ -0,0 +1,38 @@
+// Modulo para recoger el valor de lo que venga del input
+//Helpers (comunes para todo el boletín)
+
+function $inputValue(id: string) : string{
+    const input = document.getElementById(id) as HTMLInputElement; //Lectura
+    let result = "";
+
+    if(input){
+        result = input.value; //Recojo el valor (aqui es donde leo)
+    }
+
+    return result;
+}
+
+function $writeNode (id: string, msg: string) : void {
+    const nodo = document.getElementById(id) as HTMLElement; //Escritura
+
+    if(nodo){
+        nodo.textContent = msg;
+    }
+
+}
+
+
+function downloader() : void {
+    const regexp = new RegExp("^https:\/\/"); // Se escapa las barras y no se pone nada mas para que obligaotriamente tenga que poner https:// + lo que sea
+    const url = $inputValue("url");
+
+    if(regexp.test(url)){
+        window.location.href = url;
+
+    }else{
+        $writeNode("error", "URL no valida, introduce otra");
+        window.setTimeout(() => $writeNode("error", ""), 5000);
+    }
+
+}
+
